11cd00c5efbcce1fa5aa43878518761cc0f6f449
Oculareo <oculareo@gmail.com>
2022-10-26 15:23:23 -0400

    Added and configured ts-jest

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
+
+const Home: NextPage = () => {
+	return (
+		<>
+			<h1>Level Up</h1>
+		</>
+	);
+};
+
+export default Home;
