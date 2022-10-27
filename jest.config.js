c7941a0729156486be6f3898d1c09dbbe52286b5
Oculareo <oculareo@gmail.com>
2022-10-26 20:46:31 -0400

    Added and configured react-testing-library

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+	preset: 'ts-jest',
+	testEnvironment: 'jsdom',
+	transform: {
+		'^.+\\.[tj]sx?$': [
+			'ts-jest',
+			{
+				tsconfig: 'tsconfig.jest.json',
+			},
+		],
+	},
+	setupFilesAfterEnv: ['./src/jest.setup.ts'],
 };
---
+++ src/components/Hello.spec.tsx
@@ -0,0 +1,8 @@
+import { Hello } from './Hello';
+import { render, screen } from '@testing-library/react';
+
+it('Renders Hello World', () => {
+	render(<Hello />);
+	const myElement = screen.getByText('Hello World!');
+	expect(myElement).toBeInTheDocument();
+});
