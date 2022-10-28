7504651440e72134c368102773fe471dc18c1740
Oculareo <oculareo@gmail.com>
2022-10-27 22:08:29 -0400

    Added coverage threshold

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,24 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'jsdom',
-	transform: {
-		'^.+\\.[tj]sx?$': [
-			'ts-jest',
-			{
-				tsconfig: 'tsconfig.jest.json',
-			},
-		],
-	},
-	setupFilesAfterEnv: ['./src/jest.setup.ts'],
+  preset: "ts-jest",
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.[tj]sx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.jest.json",
+      },
+    ],
+  },
+  setupFilesAfterEnv: ["./src/jest.setup.ts"],
+  collectCoverage: true,
+  collectCoverageFrom: ["./src/**"],
+  coverageThreshold: {
+    global: {
+      lines: 100,
+      branches: 100,
+      functions: 100,
+      statements: 100,
+    },
+  },
 };
