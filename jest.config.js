2c490ad7d0cdde58b38cbca8e5f9554c2bcfe876
Oculareo <oculareo@gmail.com>
2022-10-27 22:21:54 -0400

    Temporarily adjusted jest global coverage for CI

--- jest.config.js
+++ jest.config.js
@@ -22,3 +22,12 @@ module.exports = {
     },
   },
 };
+
+// coverageThreshold: {
+//     global: {
+//       lines: 100,
+//       branches: 100	,
+//       functions: 100,
+//       statements: 100,
+//     },
+//   },
